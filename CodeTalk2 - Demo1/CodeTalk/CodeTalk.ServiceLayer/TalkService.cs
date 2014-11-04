aa8ebbcc444b35587ebb09df2d19c47ba394545f
Greg Pugh <gpugh@nc.rr.com>
2014-11-03 21:17:00 -0500

    CodeTalk2 - Demo1
    CodeTalk2 - Demo1CodeTalk2 - Demo1

---
+++ CodeTalk2 - Demo1/CodeTalk/CodeTalk.ServiceLayer/TalkService.cs
@@ -0,0 +1,27 @@
+﻿using CodeTalk.DataSource.Repositories;
+using CodeTalk.Domain.Contracts.Services;
+using CodeTalk.Domain.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTalk.ServiceLayer
+{
+    public class TalkService : ITalkService
+    {
+        public IList<Talk> GetTalks()
+        {
+            var repo = new TalkRepository();
+            return repo.GetTalks().ToList();
+        }
+
+        public bool AddTalk(Talk newTalk)
+        {
+            var repo = new TalkRepository();
+            return repo.AddTalk(newTalk);
+        }
+    }
+}
