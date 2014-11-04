0b97ab0c9ccf7226ed9cb8d920e392f42035cba8
Greg Pugh <gpugh@nc.rr.com>
2014-11-03 21:21:58 -0500

    CodeTalk3-Demo1
    CodeTalk3-Demo1

---
+++ CodeTalk3 - Demo1/CodeTalk/CodeTalk/Controllers/TalkController.cs
@@ -0,0 +1,41 @@
+﻿using CodeTalk.Domain.Models;
+using CodeTalk.ServiceLayer;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeTalk.Controllers
+{
+    public class TalkController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var talkService = new TalkService();
+            var talks = talkService.GetTalks();
+
+            return View(talks);
+        }
+
+        public ActionResult Insert()
+        {
+            var talk = new Talk();
+            return View(talk);
+        }
+
+        [HttpPost]
+        public ActionResult Insert(Talk newTalk)
+        {
+            var talkService = new TalkService();
+            talkService.AddTalk(newTalk);
+            return View();
+        }
+       
+
+
+
+    }
+}
